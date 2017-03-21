78c57fa3ad116383b26aa13f3af4ae962c121016
Blaise Gauvin St-Denis <st-denis.blaise@ouranos.ca>
2017-03-21 14:01:43 -0400

    Introduce logging propagation

--- wps_processes/wps_pavicrawler.py
+++ wps_processes/wps_pavicrawler.py
@@ -19,7 +19,7 @@ else:
 
 # Example usage:
 # localhost/pywps?service=WPS&request=execute&version=1.0.0&\
-# identifier=pavicrawler
+# identifier=pavicrawler&storeExecuteResponse=true&status=true&DataInputs=
 
 # Current behaviour: values in the NetCDF files take precedence over the
 # values in the Solr database. This could be an option as an input...
@@ -40,6 +40,8 @@ json_output_path = configuration.get_config_value('server', 'outputpath')
 
 json_format = get_format('JSON')
 gmlxml_format = get_format('GML')
+text_format = get_format('TEXT')
+
 
 # Fix for OpenStack internal/external ip:
 # (the internal ip is the environment variable OPENSTACK_INTERNAL_IP)
@@ -57,8 +59,12 @@ class PavicsCrawler(Process):
                                max_occurs=10000)]
         outputs = [ComplexOutput('crawler_result',
                                  'PAVICS Crawler Result',
-                                 supported_formats=[json_format])]
-        outputs[0].as_reference = True
+                                 supported_formats=[json_format],
+                                 as_reference=True),
+                   ComplexOutput('log_file',
+                                 'Log file',
+                                 supported_formats=[text_format],
+                                 as_reference=True)]
 
         super(PavicsCrawler, self).__init__(
             self._handler,
@@ -71,6 +77,25 @@ class PavicsCrawler(Process):
             status_supported=True)
 
     def _handler(self, request, response):
+        # Here we construct a unique filename
+        time_str = time.strftime("%Y-%m-%dT%H:%M:%SZ",time.gmtime())
+        log_file_name = "log_file_%s_" % (time_str,)
+        log_file = os.path.join(output_path, log_file_name)
+        import logging
+        from logging.config import dictConfig
+        logger = logging.getLogger(__name__)
+        lf = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        dictConfig({'version':1,
+                    'disable_existing_loggers': False,
+                    'formatters': {'standard': {'format': lf}},
+                    'handlers': {'logfile': {'level': 'INFO',
+                                             'class': 'logging.FileHandler',
+                                             'filename': log_file,
+                                             'formatter': 'standard'}},
+                    'loggers': {'': {'handlers': ['logfile'],
+                                     'level': 'INFO',
+                                     'propagate': True}}})
+
         if 'target_files' in request.inputs:
             target_files = []
             for i in range(len(request.inputs['target_files'])):
@@ -78,12 +103,17 @@ class PavicsCrawler(Process):
         else:
             # workaround for poor handling of default values
             target_files = None
-        update_result = catalog.pavicrawler(
-            thredds_server, solr_server, my_facets, set_dataset_id=True,
-            internal_ip=internal_ip, external_ip=external_ip,
-            output_internal_ip=True,
-            wms_alternate_server=wms_alternate_server,
-            target_files=target_files)
+
+        try:
+            update_result = catalog.pavicrawler(
+                thredds_server, solr_server, my_facets, set_dataset_id=True,
+                internal_ip=internal_ip, external_ip=external_ip,
+                output_internal_ip=True,
+                wms_alternate_server=wms_alternate_server,
+                target_files=target_files)
+        except:
+            logger.error('Function raised an Exception', exc_info=True)
+            update_result = json.dumps({'Exception': 'check log file'})
 
         # Here we construct a unique filename
         time_str = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
@@ -94,4 +124,6 @@ class PavicsCrawler(Process):
         f1.close()
         response.outputs['crawler_result'].file = output_file
         response.outputs['crawler_result'].output_format = json_format
+        response.outputs['log_file'].file = log_file
+        response.outputs['log_file'].output_format = text_format
         return response
