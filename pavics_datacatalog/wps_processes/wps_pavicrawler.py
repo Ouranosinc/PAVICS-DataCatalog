a5613194ebd44a142fd4ef7037417700a27af09c
David Huard <david.huard@gmail.com>
2017-10-05 12:43:00 -0400

    abstract details

--- pavics_datacatalog/wps_processes/wps_pavicrawler.py
+++ pavics_datacatalog/wps_processes/wps_pavicrawler.py
@@ -66,7 +66,7 @@ class PavicsCrawler(Process):
             self._handler,
             identifier='pavicrawler',
             title='PAVICS Crawler',
-            abstract='Crawl thredds server and write to database.',
+            abstract='Crawl thredds server and write metadata to SOLRabtra database.',
             version='0.1',
             inputs=inputs,
             outputs=outputs,
